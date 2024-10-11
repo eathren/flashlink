f5b6130be35da24501a7b9deabd3c5c2799a8312
nolanbraman <nolan.braman@gmail.com>
2024-10-11 02:17:24 -0700

    layout

---
+++ src/features/free/stores/use-free-store.tsx
@@ -0,0 +1,55 @@
+import { create } from "zustand"
+import { createJSONStorage, persist } from "zustand/middleware"
+
+interface FormData {
+  name: string
+  email: string
+  phone: string
+  linkedin?: string
+  discord?: string
+  website?: string
+  address?: string
+  company?: string
+  bio?: string
+}
+
+export enum Layout {
+  left = "left",
+  center = "center",
+  right = "right",
+}
+
+interface CardStore {
+  formData: FormData
+  setFormData: (data: Partial<FormData>) => void
+  layout: Layout
+  setLayout: (layout: Layout) => void
+}
+
+const useCardStore = create<CardStore>()(
+  persist(
+    (set) => ({
+      formData: {
+        name: "",
+        email: "",
+        phone: "",
+        linkedin: "",
+        discord: "",
+        website: "",
+        address: "",
+        company: "",
+        bio: "",
+      },
+      setFormData: (data) =>
+        set((state) => ({ formData: { ...state.formData, ...data } })),
+      layout: Layout.left,
+      setLayout: (layout) => set({ layout }),
+    }),
+    {
+      name: "card-storage",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+)
+
+export default useCardStore
--- src/routes/create-free.tsx
+++ src/routes/create-free.tsx
@@ -1,38 +1,15 @@
-import { Button } from "@/components/ui/button"
-import { createFileRoute, Link } from "@tanstack/react-router"
-import html2canvas from "html2canvas"
-import { useRef } from "react"
+import { createFileRoute } from "@tanstack/react-router"
+import { lazy, Suspense } from "react"
+
+const CreateFreeCard = lazy(
+  () => import("@/features/free/components/create-free-card")
+)
 
 function CreateFree() {
-  const cardRef = useRef(null)
-
-  const downloadCard = () => {
-    if (cardRef.current) {
-      html2canvas(cardRef.current).then((canvas) => {
-        const link = document.createElement("a")
-        link.href = canvas.toDataURL("image/png")
-        link.download = "business-card.png"
-        link.click()
-      })
-    }
-  }
-
   return (
-    <div className="p-2">
-      <div ref={cardRef} className="border p-4 w-80 mx-auto">
-        <h4>Your Business Card</h4>
-        <p>Name: Your Name</p>
-        <p>Email: your.email@example.com</p>
-        <p>Phone: (123) 456-7890</p>
-        <p>LinkedIn: linkedin.com/in/yourprofile</p>
-        <p>Discord: yourdiscord#1234</p>
-        {/* Add more customizable fields here */}
-      </div>
-      <Button onClick={downloadCard}>Create and Download Free Card</Button>
-      <Link to="/sign-up">
-        <Button>Sign Up for an Account</Button>
-      </Link>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <CreateFreeCard />
+    </Suspense>
   )
 }
 
