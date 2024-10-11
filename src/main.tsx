755133a9c0765c063cddd8f1174a6790c4ba83c3
nolanbraman <nolan.braman@gmail.com>
2024-10-10 22:44:17 -0700

    router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode, Suspense } from "react"
+import ReactDOM from "react-dom/client"
+import { RouterProvider, createRouter } from "@tanstack/react-router"
+import "./index.css"
+import { QueryClient } from "@tanstack/react-query"
+import { routeTree } from "./routeTree.gen"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient()
+
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient: queryClient,
+    auth: undefined!,
+  },
+  defaultPreload: "intent",
+  // This will ensure that the loader is always called when the route is preloaded or visited
+  defaultPreloadStaleTime: 0,
+})
+
+function App() {
+  return <RouterProvider router={router} context={{ queryClient }} />
+}
+
+const rootElement = document.getElementById("root")!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
+    </StrictMode>
+  )
+}
